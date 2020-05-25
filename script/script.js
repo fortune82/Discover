0afb84579c97e1cd7ce4c94f6dc9beaf0de8cfbf
fortuna8.2 <fortuna8228@gmail.com>
2020-05-25 22:35:42 +0300

    закончил сайт

--- script/script.js
+++ script/script.js
@@ -4,3 +4,18 @@ $(document).ready(function () {
         dots: true
     });
 });
+
+$('.mini-menu').click(function () {
+    if ($('nav ul').hasClass('active')) {
+        $('nav ul').removeClass('active');
+    } else {
+        $('nav ul').addClass('active');
+    }
+});
+
+$('nav a').click(function () {
+    $('nav ul').removeClass('active');
+})
+$('.back').click(function () {
+    $('nav ul').removeClass('active');
+})
